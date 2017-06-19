ea763ec239731c2107794ac303b4133e8ebc8a73
festinuz <festinuz@gmail.com>
2017-06-19 19:11:51 +0300

    Initial commit, just starting to learn reddit API.

---
+++ main.py
@@ -0,0 +1,19 @@
+import praw
+from data.bot_data import DATA
+from selftext import SELFTEXT
+
+
+reddit = reddit = praw.Reddit(client_id=DATA['client_id'],
+                              client_secret=DATA['client_secret'],
+                              user_agent=DATA['user_agent'],
+                              username=DATA['username'],
+                              password=DATA['password'])
+
+print(reddit.read_only)
+subreddits = {
+    'test': 'https://www.reddit.com/r/test/',
+}
+
+subreddit = reddit.subreddit('test')
+subreddit.submit('definitive bot testing', selftext=SELFTEXT)
+# help(subreddit)
---
+++ selftext.py
@@ -0,0 +1,12 @@
+# Contains a text for post in 'markdown'
+
+SELFTEXT = '''
+This is a test post that was  made by a bot to test formatting and stuff!
+
+# **TABLE 1**
+
+Questions|Answers
+:--|:--
+What is your favorite color?|Orange.
+Cats or dogs?|Foxes.
+'''
