09763f5940f020783901ed7767b460215d05a634
festinuz <festinuz@gmail.com>
2017-06-22 22:15:13 +0300

    +Make reddit API asyncronous-ish

--- cmcb/reddit.py
+++ cmcb/reddit.py
@@ -1,4 +1,4 @@
-
+import asyncio
 import praw
 
 
@@ -7,11 +7,14 @@ class RateRedditAPI:
         self._reddit = praw.Reddit(
           client_id=client_id, client_secret=client_secret,
           user_agent=user_agent, username=username, password=passwd)
+        self.loop = asyncio.get_event_loop()
+
+    async def get_submission(self, submission_id):
+        return await self.loop.run_in_executor(
+          self._reddit.submission(id=submission_id))
 
     async def get_top_level_comments(self, submission_id):
-        submission = self._reddit.submission(id=submission_id)
-        return submission.comments
+        return self.get_submission(submission_id).comments
 
     async def edit_submission(self, submission_id, updated_text):
-        submission = self._reddit.submission(id=submission_id)
-        submission.edit(updated_text)
+        return self.get_submission(submission_id).comments
