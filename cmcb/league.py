adbfd1af2dad8af40f267b526248d6ff39f716e6
festinuz <festinuz@gmail.com>
2017-06-25 00:41:40 +0300

    Final cache types hotfix

--- cmcb/league.py
+++ cmcb/league.py
@@ -81,9 +81,10 @@ class AsyncRateLeagueAPI:
     async def get_revision(self, region, summoner):
         output = str()
         revision_date = await self.get_summoner_revison_date(region, summoner)
-        print('Got revision date: ', revision_date, type(revision_date))
+        print('Got revision date: ', revision_date)
+        revision_date = int(revision_date)
         if revision_date is not None:
-            revision_date = revision_date / 1000
+            revision_date /= 1000
             days_ago = (time() - revision_date)//static_data.DAY
             if days_ago == 0:
                 output = 'Today'
