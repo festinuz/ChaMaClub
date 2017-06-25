f3f3811c5a37984cdd5cce1275392a303811e897
festinuz <festinuz@gmail.com>
2017-06-25 13:58:52 +0300

    Update revision date to be set as Int

--- cmcb/league.py
+++ cmcb/league.py
@@ -85,7 +85,7 @@ class AsyncRateLeagueAPI:
         revision_date = int(revision_date)
         if revision_date is not None:
             revision_date /= 1000
-            days_ago = (time() - revision_date)//static_data.DAY
+            days_ago = int((time() - revision_date)//static_data.DAY)
             if days_ago == 0:
                 output = 'Today'
             elif days_ago % 10 == 1 and days_ago != 11:
